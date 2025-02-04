926999c6f31b8245c06872e75ea1512558daf601
craw-fish <69093019+craw-fish@users.noreply.github.com>
2025-02-04 13:39:43 -0500

    feat(gui): functional "new loan" elements for LoansDialog
    Expand LoansDialog to include functioning "New Loan" section, and placeholders for other sections.
    Selection slider updates displayed values for new loan.
    Withdraw button initiates new loan with displayed parameters.

--- src/micropolisj/gui/LoansDialog.java
+++ src/micropolisj/gui/LoansDialog.java
@@ -8,13 +8,38 @@ import java.util.*;
 
 import micropolisj.engine.*;
 import static micropolisj.gui.MainWindow.formatFunds;
-import static micropolisj.gui.MainWindow.formatGameDate;
 
 public class LoansDialog extends JDialog
 {
     Micropolis engine;
 
+	JSlider selectionSlider;
+	JLabel selectionAmtLbl = new JLabel(formatFunds(0));
+	JLabel selectionCostLbl = new JLabel(formatFunds(0));
+	JLabel selectionMaxLbl = new JLabel(formatFunds(0));
+
     static ResourceBundle strings = MainWindow.strings;
+
+	private void applyChange()
+	{
+		double interest = engine.loansManager.getInterest();
+		int payPeriod = engine.loansManager.getPayPeriod();
+		int selectionAmt = ((Number) selectionSlider.getValue()).intValue();
+		int selectionMax = (int)Math.round(selectionAmt * (1 + interest));
+		int selectionCost = (int)Math.round(selectionMax / payPeriod);
+
+		if (selectionAmt >= 0) {
+			selectionAmtLbl.setText(formatFunds(selectionAmt));
+			selectionCostLbl.setText(formatFunds(selectionCost));
+			selectionMaxLbl.setText(formatFunds(selectionMax));
+		}
+
+		else {
+			selectionAmtLbl.setText(formatFunds(0));
+			selectionCostLbl.setText(formatFunds(0));
+			selectionMaxLbl.setText(formatFunds(0));
+		}
+	}
 
     public LoansDialog(Window owner, Micropolis engine)
     {
@@ -28,42 +53,14 @@ public class LoansDialog extends JDialog
 		mainBox.setBorder(BorderFactory.createEmptyBorder(8,8,8,8));
 		add(mainBox, BorderLayout.CENTER);
 
+        // add dialog sections (panes), separated by horizontal lines
+		mainBox.add(makeNewLoanPane());
+		mainBox.add(new JSeparator(SwingConstants.HORIZONTAL));
+        mainBox.add(makeActiveLoansPane());
+		mainBox.add(new JSeparator(SwingConstants.HORIZONTAL));
+		mainBox.add(makeFinalizePane());
 
-        // *add dialog sections (panes), separated by horizontal lines
-
-        // mainBox.add(makeEXAMPLE1Pane());
-
-		// JSeparator sep = new JSeparator(SwingConstants.HORIZONTAL);
-		// mainBox.add(sep);
-
-        // mainBox.add(makeEXAMPLE2Pane());
-
-		// JSeparator sep1 = new JSeparator(SwingConstants.HORIZONTAL);
-		// mainBox.add(sep1);
-
-
-        // *add buttons at bottom of dialog
-
-        // JPanel buttonPane = new JPanel();
-        // add(buttonPane, BorderLayout.SOUTH);
-
-        // JButton EXAMPLE1Btn = new JButton(strings.getString("loansdlg.EXAMPLE1"));
-		// EXAMPLE1Btn.addActionListener(new ActionListener() {
-		// 	public void actionPerformed(ActionEvent ev) {
-		// 		onEXAMPLE1Clicked();
-		// 	}});
-		// buttonPane.add(EXAMPLE1Btn);
-
-        // JButton EXAMPLE2Btn = new JButton(strings.getString("loansdlg.EXAMPLE2"));
-		// EXAMPLE2Btn.addActionListener(new ActionListener() {
-		// 	public void actionPerformed(ActionEvent ev) {
-		// 		onEXAMPLE2Clicked();
-		// 	}});
-		// buttonPane.add(EXAMPLE2Btn);
-
-
-
-        // boilerplate dialog stuff
+        // dialog boilerplate
         setAutoRequestFocus_compat(false);
         pack();
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -76,7 +73,131 @@ public class LoansDialog extends JDialog
 			JComponent.WHEN_IN_FOCUSED_WINDOW);
     }
 
-    private void setAutoRequestFocus_compat(boolean v)
+	private JComponent makeNewLoanPane()
+	{
+		JPanel newLoanPane = new JPanel();
+		newLoanPane.setLayout(new BoxLayout(newLoanPane, BoxLayout.PAGE_AXIS));
+		// FIXME: verify/adjust dimensions
+		newLoanPane.setBorder(BorderFactory.createEmptyBorder(0,0,8,0));
+
+		// SECTION TITLE
+		JPanel titlePanel = new JPanel();
+		newLoanPane.add(titlePanel);
+
+		JLabel titleText = new JLabel(strings.getString("loansdlg.new_title"));
+		titleText.setFont(titleText.getFont().deriveFont(Font.BOLD));
+		titlePanel.add(titleText);
+
+		// INTEREST & PAY PERIOD
+		JPanel infoPanel = new JPanel();
+		newLoanPane.add(infoPanel);
+
+		JLabel infoInterest = new JLabel(strings.getString("loansdlg.new_interest"));
+		JLabel infoPeriod = new JLabel(strings.getString("loansdlg.new_period"));
+		infoPanel.add(infoInterest);
+		infoPanel.add(infoPeriod);
+
+		// LOAN SELECTION
+		JPanel selectionPanel = new JPanel();
+		newLoanPane.add(selectionPanel);
+		selectionPanel.setLayout(new GridLayout(0, 4));
+
+		// ROW 1 (headers)
+		// FIXME: column spacing too wide
+		JLabel selectionAmtHdr = new JLabel(strings.getString("loansdlg.new_amt_hdr"));
+		JLabel selectionCostHdr = new JLabel(strings.getString("loansdlg.new_cost_hdr"));
+		JLabel selectionMaxHdr = new JLabel(strings.getString("loansdlg.new_max_hdr"));
+		selectionCostHdr.setHorizontalAlignment(SwingConstants.CENTER);
+		selectionAmtHdr.setHorizontalAlignment(SwingConstants.CENTER);
+		selectionMaxHdr.setHorizontalAlignment(SwingConstants.CENTER);
+		selectionPanel.add(new JLabel(" "));
+		selectionPanel.add(selectionAmtHdr);
+		selectionPanel.add(selectionCostHdr);
+		selectionPanel.add(selectionMaxHdr);
+
+		// ROW 2 (slider & values)
+		// new loan amount slider
+		selectionSlider = new JSlider(JSlider.HORIZONTAL, 0, 1000, 0);
+		selectionSlider.setMaximum(engine.loansManager.getMaxLoan());
+
+		ChangeListener change = new ChangeListener() {
+			public void stateChanged(ChangeEvent ev) {
+				applyChange();
+			}
+		};
+
+		selectionSlider.addChangeListener(change);
+		selectionPanel.add(selectionSlider);
+
+		// new loan values
+		selectionAmtLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		selectionCostLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		selectionMaxLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		selectionPanel.add(selectionAmtLbl);
+		selectionPanel.add(selectionCostLbl);
+		selectionPanel.add(selectionMaxLbl);
+
+		// WITHDRAW BUTTON
+		JPanel withdrawPanel = new JPanel();
+		newLoanPane.add(withdrawPanel);
+
+		JButton withdrawButton = new JButton(strings.getString("loansdlg.new_withdraw_btn"));
+		withdrawButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				onWithdrawClicked();
+			}});
+		withdrawPanel.add(withdrawButton);
+
+		return newLoanPane;
+	}
+
+	private JComponent makeActiveLoansPane()
+	{
+		JPanel activeLoansPane = new JPanel();
+		activeLoansPane.setLayout(new BoxLayout(activeLoansPane, BoxLayout.PAGE_AXIS));
+		// FIXME: dimensions
+		activeLoansPane.setBorder(BorderFactory.createEmptyBorder(0,0,8,0));
+
+
+		return activeLoansPane;
+	}
+
+	private JComponent makeFinalizePane()
+	{
+		JPanel finalizePane = new JPanel();
+		finalizePane.setLayout(new BoxLayout(finalizePane, BoxLayout.PAGE_AXIS));
+		// FIXME: dimensions
+		finalizePane.setBorder(BorderFactory.createEmptyBorder(0,0,8,0));
+
+		// "pay now" amount
+
+		// "insufficient funds" message
+
+		// "continue" and "cancel" buttons
+
+		return finalizePane;
+	}
+
+    private void onWithdrawClicked()
+	{
+		int initAmt = ((Number) selectionSlider.getValue()).intValue();
+
+		engine.loansManager.startLoan(initAmt);
+		selectionSlider.setValue(0);
+		// FIXME: update active loans display
+    }
+
+	// private void onContinueClicked()
+	// {
+	// 	System.out.println("continue");
+    // }
+
+	// private void onCancelClicked()
+	// {
+	// 	System.out.println("cancel");
+    // }
+
+	private void setAutoRequestFocus_compat(boolean v)
 	{
 		try
 		{
@@ -87,14 +208,4 @@ public class LoansDialog extends JDialog
 		catch (NoSuchMethodException e) {
 		}
 	}
-
-    // private void onEXAMPLE1Clicked()
-	// {
-
-    // }
-
-    // private void onEXAMPLE2Clicked()
-	// {
-
-    // }
 }
